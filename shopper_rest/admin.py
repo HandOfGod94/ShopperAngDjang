a29b68dacddc21a719d433b33df1a59a30b29a30
HandOfGod94 <gahan94rakh@gmail.com>
2016-08-23 15:52:55 +0530

    models created

--- shopper_rest/admin.py
+++ shopper_rest/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Retailer, SaleData
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """admin view for model"""
+    list_display = ('id', 'name', 'manufacturer', 'price')
+
+
+@admin.register(Retailer)
+class RetailerAdmin(admin.ModelAdmin):
+    """# Retailer admin panel for managing retailer"""
+    list_display = ('id', 'name', 'street_address',
+                    'locality', 'city', 'contact_no')
+
+
+@admin.register(SaleData)
+class SaleDataAdmin(admin.ModelAdmin):
+    """# Product sale data for each retailer on each product"""
+    list_display = ('id', 'product_id', 'retailer_id',
+                    'bought_quantity', 'sold_quantity')
--- shopper_rest/models.py
+++ shopper_rest/models.py
@@ -1,5 +1,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    """Product table"""
+    name = models.CharField(null=False, unique=True, max_length=50)
+    manufacturer = models.CharField(null=False, max_length=50)
+    price = models.FloatField(null=False, validators=[MinValueValidator(0.0)])
+
+    def __str__(self):
+        return "<Product %s: %s>" % (self.id, self.name)
+
+
+class Retailer(models.Model):
+    """Retailer table"""
+    name = models.CharField(null=False, unique=True, max_length=50)
+    street_address = models.CharField(max_length=50)
+    locality = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    contact_no = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "<Retailer %s: %s>" % (self.id, self.name)
+
+
+class SaleData(models.Model):
+    """Schema for stoing data of each product sale from that shop"""
+    product = models.ForeignKey(Product)
+    retailer = models.ForeignKey(Retailer)
+    bought_quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    sold_quantity = models.IntegerField(validators=[MinValueValidator(0)])
